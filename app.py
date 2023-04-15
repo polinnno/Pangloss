1685342346e08be136c9ff3461383bcd5f7eb837
polinnno <polkorams@gmail.com>
2023-04-15 18:06:22 +0200

    last one

--- app.py
+++ app.py
@@ -1,12 +1,22 @@
 from flask import Flask, request
 import requests
 from message_handler import handle_message
+import logging
 
 app = Flask(__name__)
 
+logging.basicConfig(level=logging.DEBUG)
 # Replace <your-bot-token> with your actual bot token
 BOT_TOKEN = "6195203641:AAECYUhCZnzMuTkVCSsfBbE1m2IdZ2rPzLg"
 WEBHOOK_URL = "https://ba61-89-216-22-220.eu.ngrok.io"
+
+
+response = requests.get(f"https://api.telegram.org/bot{BOT_TOKEN}/getWebhookInfo")
+if response.status_code == 200:
+    print(response.json())
+else:
+    print(f"Error checking webhook status: {response.status_code} {response.reason}")
+
 
 # Set up webhook for the Telegram bot
 url = f"https://api.telegram.org/bot{BOT_TOKEN}/setWebhook?url={WEBHOOK_URL}"
@@ -17,13 +27,19 @@ if response.ok:
 else:
     print("Error setting up webhook:", response.text)
 
+
 @app.route(f"/{BOT_TOKEN}", methods=["POST"])
 def handle_bot_update():
+
     update = request.get_json()
+    logging.info(f"Received update: {update}")
+    print(update)
+    print("ok")
     message = update["message"]
     handle_message(message)
 
     return "OK"
+
 
 @app.route('/', methods=['POST'])
 def webhook():
@@ -31,21 +47,15 @@ def webhook():
 
     print("before")
     message = update["message"]
+    logging.debug(update)
     handle_message(message)
     print("after")
     if "message" in update:
         message = update["message"]
         chat_id = message["chat"]["id"]
-        print(1)
         text = message.get("text")
-
         if text == "hi":
             send_message(chat_id, "Hi <3 !")
-            handle_message(message)
-        elif text == "bye":
-            send_message(chat_id, "Goodbye!")
-        else:
-            send_message(chat_id, "I didn't understand that.")
 
     return 'OK'
 
